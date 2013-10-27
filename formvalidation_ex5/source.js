e571c46338861c88f260900773ea24745107b7af
Avinash Kaur <avinash.kaur@vinsol.com>
2013-10-28 02:44:46 +0530

    form validation ex completed

---
+++ formvalidation_ex5/source.js
@@ -0,0 +1,41 @@
+var Validations = {
+
+	init: function() {
+    return Validations.requiredFields() && Validations.checkLimit();
+	},
+
+	requiredFields: function() {
+		var elements = document.getElementsByClassName('required');
+		for(var i = 0; i < elements.length; i++) {
+			var value = elements[i].value.trim();
+			if (value == 0) {
+				alert("You cannot leave " + elements[i].name + " blank");
+				elements[i].focus();
+				return false;
+			}
+		}
+	},
+
+	checkLimit: function() {
+		var element = document.getElementById('about');
+		var size = element.value.trim().length;
+		if (size < 50) {
+			alert("You should enter minimum of 50 characters!");
+			element.focus();
+			return false;
+		}
+	},
+
+	disableButton: function() {
+		document.getElementById('submit-button').disabled = true;
+	},
+	
+  enableButton: function(element) {
+		if (element.checked) {
+			document.getElementById('submit-button').disabled = false;
+		}
+		else {
+			Validations.disableButton();
+		}
+	}
+}
