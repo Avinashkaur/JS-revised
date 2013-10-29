162d1e859772ba6494a72a9425256eda12aa75ab
Avinash Kaur <avinash.kaur@vinsol.com>
2013-10-29 23:43:32 +0530

    domain and subdomain ex completed

---
+++ domain_subdomain_ex11/source.js
@@ -0,0 +1,19 @@
+var domainSubDomain = function() {
+
+  var urlregex = /^(https?:\/\/)?((([A-z0-9]+)\.)*)([A-z0-9]+\.[A-z]{2,4})$/;
+  var url = document.getElementById("url").value;
+  var urlvalue = url.trim();
+
+  if ((urlvalue.length != 0) && (urlregex.test(urlvalue))) {
+    var sub = RegExp.$3;
+    var subdomain = sub.substr(0, sub.length-1);
+    if (subdomain.length == 0) {
+      alert("Domain: " + RegExp.$5);
+    }
+    else {
+      alert("Domain: " + RegExp.$5 + ", Subdomain: " + subdomain);
+    }
+    return true;
+  }
+  else { return false; }
+} 
