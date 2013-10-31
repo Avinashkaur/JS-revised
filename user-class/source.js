5579f31d2ae5cf76cf1462368919ff4106d6416d
Avinash Kaur <avinash.kaur@vinsol.com>
2013-10-31 12:21:02 +0530

    ex completed with changes

--- user-class/source.js
+++ user-class/source.js
@@ -1,25 +1,19 @@
 function User(name, age) {
 	this.name = name;
-	this.age = age;
+	this.age = parseInt(age, 10);
 }
 
 User.prototype.compare = function(object) {
-
 	if (this.age > object.age) {
 		alert(this.name + " is older than " + object.name);
 	}
 	else {
 		alert(object.name + " is older than " + this.name);
 	}
-
 };
 
 window.onload=function(){
-	var user1 = new User();
-	user1.name = "Sahil";
-	user1.age = 28;
-	var user2 = new User();
-	user2.name = "Karan";
-	user2.age = 26;
+	var user1 = new User("Sahil", "22");
+	var user2 = new User("Karan", 26);
 	user1.compare(user2);
 }
