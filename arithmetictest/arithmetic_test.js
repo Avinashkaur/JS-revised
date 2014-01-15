a1804e93c73fb2048bfad64aaf2f7ae3f08a0278
Avinash Kaur <avinash.kaur@vinsol.com>
2014-01-15 16:15:23 +0530

    arithmetic test updated

--- arithmetictest/arithmetic_test.js
+++ arithmetictest/arithmetic_test.js
@@ -1,13 +1,13 @@
 var ArithmeticTest = function() {
+  this.question = document.getElementById('question');
+  this.answer = document.getElementById('answer');
+  this.timer_label = document.getElementById('timer');
   this.init();
 }
 
 ArithmeticTest.prototype = {
 
   init: function() {
-    this.question = document.getElementById('question');
-    this.answer = document.getElementById('answer');
-    this.timer_label = document.getElementById('timer');
     this.timer = 10;
     this.total_questions = 10;
     this.current = 0;
@@ -16,7 +16,7 @@ ArithmeticTest.prototype = {
     this.show_time;
     this.correct = 0;
     this.unattempted = 0;
-
+    // invoke methods
     this.generateQuestions();
     this.showQuestion();   
   },
@@ -26,8 +26,8 @@ ArithmeticTest.prototype = {
   },
 
   getRandomOperator: function() {
-    var operators = ['+', '-', '*', '/'];
-    var random_number = Math.floor((Math.random() * 10) % 4);
+    var operators = ['+', '-', '*', '/'],
+        random_number = Math.floor((Math.random() * 10) % 4);
     return operators[random_number];
   },
 
@@ -44,19 +44,18 @@ ArithmeticTest.prototype = {
   },
 
   showQuestion: function() {
-    this.answer.disabled = false;
+    var ques_string = this.quiz_data[this.current].operand1 + this.quiz_data[this.current].operator + this.quiz_data[this.current].operand2;
+
     this.answer.focus();
     this.resetTimer();
     this.answer.value = "";
-    var ques_string = this.quiz_data[this.current].operand1 + this.quiz_data[this.current].operator + this.quiz_data[this.current].operand2;
     this.question.innerText = ques_string;
     this.quiz_data[this.current].testanswer = parseFloat(eval(ques_string), 10).toFixed(2);        
   },
 
   submitAnswer: function() {
-    this.answer.disabled = true;
     this.quiz_data[this.current].useranswer = this.answer.value.trim();
-    this.calculateScore();
+    this.calculateAndDisplayScore();
     this.current++;
     if (this.current < this.total_questions) {
       this.showQuestion();  
@@ -66,7 +65,7 @@ ArithmeticTest.prototype = {
     }        
   },
 
-  calculateScore: function() {
+  calculateAndDisplayScore: function() {
     var show_score = document.getElementById('score');
 
     if (parseFloat(this.quiz_data[this.current].useranswer, 10).toFixed(2) == this.quiz_data[this.current].testanswer) {
@@ -80,9 +79,10 @@ ArithmeticTest.prototype = {
   },
 
   resetTimer: function() {
+    var this_object = this;
+    
     clearInterval(this.show_time);
     this.timer = 10;
-    var this_object = this;
     this.show_time = setInterval(function() { 
       this_object.clearTimer();
     }, 1000);
@@ -94,36 +94,37 @@ ArithmeticTest.prototype = {
       this.timer_label.innerText = "Time Left: " + this.timer + " seconds";
     }
     else {
-      this.answer.disabled = true;
       clearInterval(this.show_time);
       this.submitAnswer();                
     }
   },
 
-    displayResult: function() {
-      this.timer_label.style.display = "none";
-      this.answer.style.display = "none";
-      document.getElementById('nextbutton').style.display = "none";
-      document.getElementById('score').style.display = "none";
-      
-      this.question.innerText = "Your score: " + this.correct;
-      document.getElementById('wrong').innerText = "Wrong: " + (this.total_questions - (this.correct + this.unattempted));
-      document.getElementById('unattempted').innerText = "Unattempted: " + this.unattempted;
-      var summary = "";
+  displayResult: function() {
+    var summary = "";
 
-      for (var i = 0; i < this.quiz_data.length; i++) {
-        if (this.quiz_data[i].useranswer != this.quiz_data[i].testanswer) {
-          summary = summary + "Question:" + (i + 1) + " Correct answer: " + this.quiz_data[i].testanswer + "<br>";
-        }
+    this.timer_label.style.display = "none";
+    this.answer.style.display = "none";
+    document.getElementById('nextbutton').style.display = "none";
+    document.getElementById('score').style.display = "none";
+    
+    this.question.innerText = "Your score: " + this.correct;
+    document.getElementById('wrong').innerText = "Wrong: " + (this.total_questions - (this.correct + this.unattempted));
+    document.getElementById('unattempted').innerText = "Unattempted: " + this.unattempted;
+
+    for (var i = 0; i < this.quiz_data.length; i++) {
+      if (this.quiz_data[i].useranswer != this.quiz_data[i].testanswer) {
+        summary = summary + "Question:" + (i + 1) + " Correct answer: " + this.quiz_data[i].testanswer + "<br>";
       }
-      document.getElementById('show').innerHTML = summary;
     }
+    document.getElementById('show').innerHTML = summary;
+  }
 
 }
 
 window.onload = function() {
   var next_button = document.getElementById('nextbutton');
   var quiz = new ArithmeticTest();
+
   next_button.addEventListener('click', function() { 
     quiz.submitAnswer();
   }, false);
@@ -132,6 +133,6 @@ window.onload = function() {
     if (event_object.keyCode == 13) {
       next_button.click();
     }
-  })
+  });
 
 }
